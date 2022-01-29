c53261cc84c798bacc540f7af9cc10e7f2fd192b
Alex Blair <bclan11@gmail.com>
2022-01-28 17:54:08 -0800

    placed emojis and increase and decrease emoji sizes buttons working

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [pandaSize, setPandaSize] = useState(1);
-  const [dolphinSize, setDolphinSize] = useState(1);
+  const [pandaSize, setPandaSize] = useState(2);
+  const [dolphinSize, setDolphinSize] = useState(2);
   const [isOpen, setIsOpen] = useState(false);
   const [animalArray, setAnimalArray] = useState('bat', 'squirrel');
 
+  const stylePanda = { fontSize: `${pandaSize}rem` };
+  const styleDolphin = { fontSize: `${dolphinSize}rem` };
   return (
     <div className="App">
-      
+      <section className='fight'>
+        <div className='panda'>
+          <p style={stylePanda}>🐼</p>
+          <button onClick={() => setPandaSize(pandaSize + 1)}>Panda gets stronger</button>
+          <button onClick={() => setDolphinSize(dolphinSize - 1)}>Panda attacks Dolphin</button>
+        </div>
+        <div className='dolphin'>
+          <p style={styleDolphin}>🐬</p>
+          <button onClick={() => setDolphinSize(dolphinSize + 1)}>Dolphin gets stronger</button>
+          <button onClick={() => setPandaSize(pandaSize - 1)}>Dolphin attacks panda</button>
+        </div>
+      </section>
     </div>
   );
 }
