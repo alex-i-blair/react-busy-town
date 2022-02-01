db719cae6e151082202b33629c74c81fdc57caee
Alex Blair <bclan11@gmail.com>
2022-01-31 17:56:22 -0800

    styles!

--- src/Parade.js
+++ src/Parade.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Animals from './Animals';
 
 export default function Parade({ animalArray }) {
-  return <div>
+  return <div className='animal-parade'>
     {animalArray.map((animal, i) =>
       <Animals key={animal + i} animal={animal} />)}
   </div>;
