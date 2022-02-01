b08a7af641606cdd0bcab62ecdb16e3701b8d6ea
Alex Blair <bclan11@gmail.com>
2022-01-31 17:59:01 -0800

    test-passing

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Panda/i);
+  const linkElement = screen.getByText(/Zoo Closed/i);
   expect(linkElement).toBeInTheDocument();
 });
